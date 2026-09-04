93e4046ab5ded85e4ce82e318bfb1821d2044b4a
Yaroslav Martson <martson.yaroslav@gmail.com>
2026-09-04 17:47:24 +0000

    Додано JS-логіку відображення погоди

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch('data/weather.json') 
+  .then(response => response.json()) 
+  .then(data => renderWeather(data)) 
+  .catch(error => 
+    console.error('Помилка завантаження даних:', error)); 
+  
+function renderWeather(data) { 
+  const current = document.getElementById('current-weather'); 
+  current.innerHTML = ` 
+    <h2>${data.city}</h2> 
+    <p>${data.current.temperature_c}°C, 
+    ${data.current.condition}</p>`; 
+  
+  const forecast = document.getElementById('forecast'); 
+  forecast.innerHTML = data.forecast_5_days
+  .map(day => `<p>${day.date}: ${day.temp_min_c}…${day.temp_max_c}°C, 
+${day.condition}</p>`) 
+    .join(''); 
+} 
