14a6465ab7c002232fdc4fba879e95c117ef4510
Yaroslav Martson <martson.yaroslav@gmail.com>
2026-09-07 15:55:50 +0000

    Додано конвертацію температури у Фаренгейти

--- js/script.js
+++ js/script.js
@@ -17,3 +17,7 @@ function renderWeather(data) {
 ${day.condition}</p>`) 
     .join(''); 
 } 
+function celsiusToFahrenheit(celsius) {
+    // ПОМИЛКА: правильна формула — celsius * 9/5 + 32
+    return celsius * 2 + 30;
+}
